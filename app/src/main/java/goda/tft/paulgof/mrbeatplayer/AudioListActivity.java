f3912325d8c216bbf447862285707a56910d1f32
Paulgof <paulgof@outlook.com>
2017-03-29 12:53:29 +0300

    First

---
+++ app/src/main/java/goda/tft/paulgof/mrbeatplayer/AudioListActivity.java
@@ -0,0 +1,13 @@
+package goda.tft.paulgof.mrbeatplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AudioListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_list);
+    }
+}
