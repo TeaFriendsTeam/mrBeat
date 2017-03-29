503bc1e6e740eeaea1359bae28a78bfed80d8539
Paulgof <paulgof@outlook.com>
2017-03-29 14:39:47 +0300

    Audio List

--- app/src/main/java/goda/tft/paulgof/mrbeatplayer/AudioListActivity.java
+++ app/src/main/java/goda/tft/paulgof/mrbeatplayer/AudioListActivity.java
@@ -1,13 +1,46 @@
 package goda.tft.paulgof.mrbeatplayer;
 
+import android.database.Cursor;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class AudioListActivity extends AppCompatActivity {
-
+public class AudioListActivity extends FragmentActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+    private SimpleCursorAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_list);
+        ListView listView = (ListView) findViewById(R.id.audioList);
+        adapter = new SimpleCursorAdapter(this, R.layout.audio_info, null,
+                new String[] { MediaStore.Audio.Media.DATA },
+                new int[] {
+                        R.id.textView }, 0);
+        listView.setAdapter(adapter);
+        getSupportLoaderManager().initLoader(0, null, this);
+    }
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this,
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                new String[] { MediaStore.Audio.Media._ID,
+                        MediaStore.Audio.Media.DATA },
+                null, null,
+                null);
+    }
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
     }
 }
