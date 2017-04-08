2f5a0384647e7990faddbbb0b0f341edfb50e48b
Paul Gof <paulgof@outlook.com>
2017-04-08 11:15:00 +0300

    Some improving

--- app/src/main/java/goda/tft/paulgof/mrbeatplayer/AudioListActivity.java
+++ app/src/main/java/goda/tft/paulgof/mrbeatplayer/AudioListActivity.java
@@ -4,25 +4,16 @@ import android.Manifest;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
-import android.os.PersistableBundle;
 import android.provider.MediaStore;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -35,7 +26,7 @@ public class AudioListActivity extends AppCompatActivity {
     ArrayList<Audio> audioList;
     AudioPlayer audioPlayer = new AudioPlayer();
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -114,7 +105,7 @@ public class AudioListActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.menuTitle:
-                //TODO
+                //TODO randomize AudioList
                 Toast toast = Toast.makeText(getApplicationContext(), "Rand", Toast.LENGTH_SHORT);
                 toast.show();
                 return true;
